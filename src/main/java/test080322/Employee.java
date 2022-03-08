b4aca3a4eabde1eae28610dff4c995fd95757df8
LiamNilan599 <Lmmt.2k36@gmail.com>
2022-03-08 10:12:49 +0000

    Valid PPS

--- src/main/java/test080322/Employee.java
+++ src/main/java/test080322/Employee.java
@@ -1,4 +1,78 @@
 package test080322;
 
-public class Employee {
+public class Employee
+{
+    String title, name, PPS, phone, type;
+    int age;
+
+    Employee(String title, String name, String PPS, String phone, String type, int age)
+    {
+     this.title = title ;
+
+     if(name.length() < 6 || name.length() > 22)
+     {
+         String Message = "Name invalid";
+         throw new IllegalArgumentException(Message);
+     }
+     else if(PPS.length() != 6)
+     {
+         String Message = "PPS invalid";
+         throw new IllegalArgumentException(Message);
+     }
+    this. name = name;
+    this. PPS = PPS;
+    this.phone = phone;
+    this.type = type;
+    this.age = age;
+
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPPS() {
+        return PPS;
+    }
+
+    public void setPPS(String PPS) {
+        this.PPS = PPS;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/test/java/test080322/EmployeeTest.java
+++ src/test/java/test080322/EmployeeTest.java
@@ -1,4 +1,28 @@
 package test080322;
 
-public class EmployeeTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeTest
+{
+    @Test
+    void ValidTitle()
+    {
+        Employee test = new Employee("Mrs","Brandy","345fds","0984321", "Full-time", 24);
+        assertEquals("Mrs", test.getTitle());
+    }
+
+    @Test
+    void ValidName()
+    {
+        Employee test = new Employee("Mrs","Brandy","345fds","0984321", "Full-time", 24);
+        assertEquals("Brandy", test.getName());
+    }
+    @Test
+    void Validpps()
+    {
+        Employee test = new Employee("Mrs","Brandy","345fds","0984321", "Full-time", 24);
+        assertEquals("345fds", test.getPPS());
+    }
 }
